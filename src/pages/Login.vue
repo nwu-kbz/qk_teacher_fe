bdd6998be62269ee8c5dba1fc60b637da09a692b
Your Name <you@example.com>
2019-02-28 21:49:02 +0800

    login

---
+++ src/pages/Login.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="main">
+    <div class="bg bg-blur"></div>
+
+    <div class="main-page">
+      <div class="title">
+        <h1 style="font-size: 30px;color: #51d3b2;display: inline-block">用户登录&nbsp;</h1>
+        <span style="font-size: 15px;color:#dbdbdb;">Login User</span></div>
+      <div class="welcomeText">
+        <span class="line"></span>
+        <span class="txt" style="font-size: 10px">欢迎来到轻课</span>
+        <span class="line"></span>
+      </div>
+      <Input class='input_message' prefix="ios-contact-outline" placeholder="用户名" style="width: auto" clearable/>
+      <Input class='input_message' prefix="ios-lock-outline" placeholder="密码" style="width: auto" clearable type="password"/>
+      <Button type="success" long class="login_btn">登录</Button>
+      <div class="altOption">
+        <div class="remenberPassword altDiv">
+          <Checkbox v-model="rememberPasswd">记住密码</Checkbox>
+        </div>
+        <div class="forgetPassword altDiv">
+          <Icon type="ios-help-circle" size="18"/>忘记密码
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { Checkbox,Icon,Input,Button} from 'iview';
+  export default {
+    name: "Login",
+    components:{Checkbox,Icon,Input,Button },
+    data() {
+      return {
+        rememberPasswd:false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .main {
+    height: 100%;
+    width: 100%;
+  }
+
+  .bg {
+    background: url("../assets/bg.jpg");
+    height: 100%;
+    width: 100%;
+    text-align: center;
+  }
+
+  .bg-blur {
+    float: left;
+    width: 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    -webkit-filter: blur(5px);
+    -moz-filter: blur(5px);
+    -o-filter: blur(5px);
+    -ms-filter: blur(5px);
+    filter: blur(5px);
+  }
+  .main-page .title{
+      margin: 0 auto;
+      text-align: center;
+    }
+  .main-page {
+    width: 400px;
+    height: 300px;
+    background-color: white;
+    padding: 3px 25px 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    margin-left: -225px;
+    margin-top: -180px;
+
+  }
+  .main {
+    position: relative;
+  }
+  .welcomeText {
+    height: 20px;
+    line-height: 20px;
+    text-align: center;
+  }
+
+  .welcomeText .line {
+    display: inline-block;
+    width: 115px;
+    border-top: 1px solid #ccc;
+  }
+
+  .welcomeText .txt {
+    color: #686868;
+    vertical-align: middle;
+  }
+.login_btn{
+  background-color: #51d3b2;
+  border:none;
+  width: 300px;
+  display: block !important;
+  margin:  0 auto;
+}
+  .input_message{
+
+    display: block;
+    width: 300px!important;
+
+    margin: 10px auto;
+  }
+.altDiv{
+  display: inline-block;
+}
+.forgetPassword a{
+  line-height: 25px;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '../pages/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {  //默认页面
+      path:'/',
+      redirect:'/login',
+
+
     }
   ]
 })
